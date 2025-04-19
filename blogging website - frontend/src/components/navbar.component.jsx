51f012f92a8572d69f5e9889b357c5e0e19a473f
mdarrakibislamrifat <rohanrfat43@gmail.com>
2025-04-19 17:26:48 +0600

    Navbar design complete

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,20 +1,49 @@
 import { Link } from "react-router";
 import logo from "../imgs/logo.png";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [searchBoxVisibility, setSeachBoxVisibility] = useState(false);
+
   return (
     <nav className="navbar">
       <Link to="/" className="flex-none w-10">
-        <img src={logo} alt="" className="w-fullgit branch -M main" />
+        <img src={logo} alt="Logo" className="w-full" />
       </Link>
 
-      <div className="absolute w-full bg-white left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw]">
+      <div
+        className={
+          "absolute w-full bg-white left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto" +
+          (searchBoxVisibility ? " show" : " hide") +
+          " md:opacity-100 md:pointer-events-auto"
+        }
+      >
         <input
           type="text"
           placeholder="Search"
-          className="w-full md:w-auto bg-grey p-4 pl-6 md:pr-6 rounded-full placeholder:text-dark-grey"
+          className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
         />
-        <i className="fi fi-rr-search"></i>
+        <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
+      </div>
+
+      <div className="flex items-center gap-3 md:gap-6 ml-auto">
+        <button
+          className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"
+          onClick={() => setSeachBoxVisibility((currentVal) => !currentVal)}
+        >
+          <i className="fi fi-rr-search text-xl"></i>
+        </button>
+
+        <Link className="hidden md:flex gap-2" to="/editor">
+          <i class="fi fi-rr-file-edit"></i>
+          Write
+        </Link>
+        <Link className="btn-dark py-2" to="/signin">
+          Sign In
+        </Link>
+        <Link className="btn-light py-2 hidden md:block" to="/signup">
+          Sign Up
+        </Link>
       </div>
     </nav>
   );
