495b2f95aeb5ff95d8711ff34fd9298b40bffba1
mdarrakibislamrifat <rohanrfat43@gmail.com>
2025-04-20 18:29:43 +0600

    Signin and Signup page design

--- blogging website - frontend/src/components/input.component.jsx
+++ blogging website - frontend/src/components/input.component.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 export default function InputBox({ name, type, id, value, placeholder, icon }) {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
   return (
     <div className="relative w-[100%] mb-4">
       <input
         name={name}
-        type={type}
+        type={type == "password" && passwordVisible ? "text" : type}
         id={id}
         defaultValue={value}
         placeholder={placeholder}
         className="input-box"
       />
       <i className={`fi ${icon} input-icon`}></i>
+      {type === "password" ? (
+        <i
+          className={
+            "fi fi-rr-eye" +
+            (passwordVisible ? "-crossed" : "") +
+            " input-icon right-4 left-auto cursor-pointer"
+          }
+          onClick={() => setPasswordVisible((currentVal) => !currentVal)}
+        ></i>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,4 +1,6 @@
 import InputBox from "../components/input.component";
+import googleIcon from "../imgs/google.png";
+import { Link } from "react-router-dom";
 
 export default function UserAuthForm({ type }) {
   return (
@@ -9,7 +11,7 @@ export default function UserAuthForm({ type }) {
         </h1>
         {type !== "sign-in" ? (
           <InputBox
-            name="Fullname"
+            name="fullname"
             type="text"
             placeholder="Full Name"
             icon="fi-rr-user"
@@ -17,6 +19,48 @@ export default function UserAuthForm({ type }) {
         ) : (
           ""
         )}
+
+        <InputBox
+          name="email"
+          type="email"
+          placeholder="Email"
+          icon="fi-br-envelope"
+        />
+        <InputBox
+          name="password"
+          type="password"
+          placeholder="Password"
+          icon="fi-ss-key"
+        />
+
+        <button className="btn-dark center mt-14" type="submit">
+          {type.replace("-", " ")}
+        </button>
+        <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+          <hr className="w-1/2 border-black" />
+          <p>or</p>
+          <hr className="w-1/2 border-black" />
+        </div>
+        <button className="btn-dark flex items-center justify-center gap-4 w-[90%] cener">
+          <img src={googleIcon} alt="" className="w-5" />
+          continue with google
+        </button>
+
+        {/* {type == "sign-in" ? (
+          <p className="mt-6 text-dark-grey text-xl text-center">
+            Don't have an account?
+            <Link to="/signup" className="underline text-black text-xl ml-1">
+              Join us today.
+            </Link>
+          </p>
+        ) : (
+          <p className="mt-6 text-dark-grey text-xl text-center">
+            Already have an account?
+            <Link to="/signin" className="underline text-black text-xl ml-1">
+              Sign in here.
+            </Link>
+          </p>
+        )} */}
       </form>
     </section>
   );
