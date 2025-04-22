65ff43236c1ffb803cca0b695f470bc4a7f344af
mdarrakibislamrifat <rohanrfat43@gmail.com>
2025-04-22 21:51:33 +0600

    Solve the signup issues

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,15 +1,34 @@
 import { Route, Routes } from "react-router";
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
+import { createContext, useEffect } from "react";
+import { lookInSession } from "./common/session";
+import { useState } from "react";
+
+export const UserContext = createContext({});
 
 const App = () => {
+  const [userAuth, setUserAuth] = useState();
+
+  useEffect(() => {
+    const userInSession = lookInSession("user");
+
+    if (userInSession) {
+      setUserAuth(JSON.parse(userInSession));
+    } else {
+      setUserAuth(null);
+    }
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<Navbar />}>
-        <Route path="signin" element={<UserAuthForm type="sign-in" />} />
-        <Route path="signup" element={<UserAuthForm type="sign-up" />} />
-      </Route>
-    </Routes>
+    <UserContext.Provider value={{ userAuth, setUserAuth }}>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+          <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+        </Route>
+      </Routes>
+    </UserContext.Provider>
   );
 };
 
--- blogging website - frontend/src/pages/userAuthFormInner.jsx
+++ blogging website - frontend/src/pages/userAuthFormInner.jsx
@@ -1,19 +1,27 @@
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
 import { Link } from "react-router";
 import { toast, Toaster } from "react-hot-toast";
 import axios from "axios";
+import { storeInSession } from "../common/session";
+import { UserContext } from "../App";
 
 export default function UserAuthFormInner({ type }) {
   const authForm = useRef();
+  const { userAuth, setUserAuth } = useContext(UserContext);
+
+  console.log(userAuth?.access_token); // এখানে দেখো access_token আছে কিনা
+
+  console.log(userAuth); // এখানে দেখো access_token আছে কিনা
 
   const userAuthThroughServer = (serverRoute, formData) => {
     axios
       .post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
       .then(({ data }) => {
-        console.log(data);
+        storeInSession("user", JSON.stringify(data));
+        console.log(sessionStorage);
       })
       .catch((error) => {
         console.log("🔥 ERROR RESPONSE:", error.response);
