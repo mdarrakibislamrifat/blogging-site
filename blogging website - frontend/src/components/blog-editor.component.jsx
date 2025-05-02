0cc1b9178a921a4076b53ed30bebad95b61f1941
mdarrakibislamrifat <rohanrfat43@gmail.com>
2025-05-02 16:28:16 +0600

    hosting image in imagebb

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -2,10 +2,37 @@ import { Link } from "react-router";
 import logo from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png";
+import { useRef } from "react";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
 export default function BlogEditor() {
-  const handleBannerUpload = (e) => {
-    let img = e.target.files[0];
-    console.log(img);
+  let blogBannerRef = useRef();
+
+  const handleBannerUpload = async (e) => {
+    const imgFile = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", imgFile);
+
+    try {
+      const res = await fetch(image_hosting_api, {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await res.json();
+      console.log(data);
+
+      if (data.success) {
+        const imageUrl = data.data.url;
+        blogBannerRef.current.src = imageUrl; // Update banner src here!
+      } else {
+        console.error("Upload failed: ", data.error);
+      }
+    } catch (error) {
+      console.error("Upload failed:", error);
+    }
   };
 
   return (
@@ -27,7 +54,12 @@ export default function BlogEditor() {
           <div className="mx-auto max-w[900px]w-full ">
             <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
               <label htmlFor="uploadBanner">
-                <img src={defaultBanner} alt="" className="z-20" />
+                <img
+                  ref={blogBannerRef}
+                  src={defaultBanner}
+                  alt=""
+                  className="z-20"
+                />
                 <input
                   id="uploadBanner"
                   type="file"
