88e6a9655fce61ad0b3aeca1ff5bd8c091147ce4
mdarrakibislamrifat <rohanrfat43@gmail.com>
2025-05-02 15:17:35 +0600

    Blog editor banner

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -1,12 +1,45 @@
 import { Link } from "react-router";
 import logo from "../imgs/logo.png";
+import AnimationWrapper from "../common/page-animation";
+import defaultBanner from "../imgs/blog banner.png";
 export default function BlogEditor() {
+  const handleBannerUpload = (e) => {
+    let img = e.target.files[0];
+    console.log(img);
+  };
+
   return (
-    <nav className="navbar">
-      <Link to="/" className="flex-none w-10">
-        <img src={logo} alt="" />
-      </Link>
-      <div></div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <Link to="/" className="flex-none w-10">
+          <img src={logo} alt="" />
+        </Link>
+
+        <p className="max-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+        <div className="flex gap-4 ml-auto ">
+          <button className="btn-dark py-2">Publish</button>
+          <button className="btn-light py-2">Save Draft</button>
+        </div>
+      </nav>
+
+      <AnimationWrapper>
+        <section>
+          <div className="mx-auto max-w[900px]w-full ">
+            <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
+              <label htmlFor="uploadBanner">
+                <img src={defaultBanner} alt="" className="z-20" />
+                <input
+                  id="uploadBanner"
+                  type="file"
+                  accept=".png,.jpg,.jpeg"
+                  hidden
+                  onChange={handleBannerUpload}
+                />
+              </label>
+            </div>
+          </div>
+        </section>
+      </AnimationWrapper>
+    </>
   );
 }
