f8fc3cb828fd4cd365df31135767c7ff7f012af0
mdarrakibislamrifat <rohanrfat43@gmail.com>
2025-05-03 13:00:32 +0600

    Bloger foxrd

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -26,7 +26,7 @@ export default function BlogEditor() {
     setTextEditor(
       new EditorJS({
         holderId: "textEditor",
-        data: "",
+        data: content,
         tools: tools,
         placeholder: "Let's write an awesome story!",
       })
@@ -144,6 +144,7 @@ export default function BlogEditor() {
               </label>
             </div>
             <textarea
+              defaultValue={title}
               placeholder="Blog Title"
               className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
               onKeyDown={handleTitleKeyDown}
--- blogging website - frontend/src/components/publish-form.component.jsx
+++ blogging website - frontend/src/components/publish-form.component.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import { Toaster, toast } from "react-hot-toast";
+import AnimationWrapper from "../common/page-animation";
+import { useContext } from "react";
+import { EditorContext } from "../pages/editor.pages";
 
 export default function PublishForm() {
-  return <div>PublishForm from component</div>;
+  let { setEditorState } = useContext(EditorContext);
+
+  const handleCloseEvent = () => {
+    setEditorState("editor");
+  };
+
+  return (
+    <AnimationWrapper>
+      <section className="">
+        <Toaster />
+        <button
+          className="w-12 h-12 absolute right-[5vw] z-10 top-[5%] lg:top-[10%]"
+          onClick={handleCloseEvent}
+        >
+          <i className="fi fi-br-cross"></i>
+        </button>
+      </section>
+    </AnimationWrapper>
+  );
 }
